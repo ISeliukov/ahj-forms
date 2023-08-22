c42eb04aee3effb7c0159b3d1eda54ee5fe89faa
Garysi <garysi@rambler.ru>
2023-08-22 13:28:54 +0300

    add latest jest

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('#ComponentContainer');
   const widget = new PopOver(container, {
     title: 'Какой-то заголовок',
-    text: 'Будьте добры, прочитайте этот текст. Этот текст очень важен для вас.',
+    text: 'Будьте добры, прочитайте этот текст. Этот текст очень важен для вас.'
   });
   widget.bindToDOM();
   // eslint-disable-next-line no-console
