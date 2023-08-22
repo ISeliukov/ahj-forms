fca3c20e9eedfff1bc2bdf43889030c76a6a8cac
Garysi <garysi@rambler.ru>
2023-08-22 13:16:35 +0300

    add ver 1.0

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/style.css';
+import './css/popover.css';
+import './js/app';
+
+// entry point for webpack
---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import PopOver from './PopOver';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('#ComponentContainer');
+  const widget = new PopOver(container, {
+    title: 'Какой-то заголовок',
+    text: 'Будьте добры, прочитайте этот текст. Этот текст очень важен для вас.',
+  });
+  widget.bindToDOM();
+  // eslint-disable-next-line no-console
+  console.log('Component started!');
+});
